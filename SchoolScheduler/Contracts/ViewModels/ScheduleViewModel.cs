c8b3dc8fc56b0344d68b2fc0072d9d7e395b2215
mzielins <matiziel10@gmail.com>
2020-11-08 19:51:31 +0100

    Add Class view

--- SchoolScheduler/Contracts/ViewModels/ScheduleViewModel.cs
+++ SchoolScheduler/Contracts/ViewModels/ScheduleViewModel.cs
@@ -2,6 +2,12 @@ using System;
 
 namespace Contracts.ViewModels {
     public class ScheduleViewModel {
-        public string[] Slots { get; set; } = new string[45];
+        public string[] Slots { get; set; }
+        public ScheduleViewModel() {
+            Slots = new string[45];
+            for (int i = 0; i < Slots.Length; i++) {
+                Slots[i] = "-";
+            }
+        }
     }
 }
---
+++ SchoolScheduler/WebClient/Utils/Descriptions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace WebClient.Utils {
+    public enum Days {
+        Monday, Tuesday, Wednesday, Thursday, Friday
+    }
+    public class Hours {
+        public static string[] Values { get; } = {
+            "8:15 - 9:00",
+            "9:15 - 10:00",
+            "10:15 - 11:00",
+            "11:15 - 12:00",
+            "12:15 - 13:00",
+            "13:15 - 14:00",
+            "14:15 - 15:00",
+            "15:15 - 16:00",
+            "16:15 - 17:00",
+        };
+
+    }
+    public class SlotToTermMapper {
+        public static ValueTuple<Days, string> GetTerm(int slot) {
+            Days day = toDaysMapper[slot % 5];
+            string hour = Hours.Values[slot / 5];
+            return (day, hour);
+        }
+        private static Dictionary<int, Days> toDaysMapper = new Dictionary<int, Days>() {
+            { 0, Days.Monday },
+            { 1, Days.Tuesday },
+            { 2, Days.Wednesday },
+            { 3, Days.Thursday },
+            { 4, Days.Friday }
+        };
+
+    }
+
+}
